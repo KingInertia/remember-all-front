b4ebcb90f9013dae80825d379b1f58f9cfb04712
KingInertia <skalyar27@gmail.com>
2025-10-30 22:21:52 +0200

    Prettier fix

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,36 +1,38 @@
 import { useEffect } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
-import StarsBg from '@/components/UI/StarsBg'
+import StarsBg from '@/components/UI/StarsBg';
 import Navbar from '@/components/UI/NavBar/NavBar';
-import { setAuthToken} from '@/store/auth/authSlice';
+import { setAuthToken } from '@/store/auth/authSlice';
 import { getUserProfile } from '@/store/userProfile/userProfileActions';
 import { useAppDispatch } from '@/hooks/hooks';
 
 const MainLayout = () => {
   const dispatch = useAppDispatch();
-const location = useLocation();
-const hideNavbarPaths = ['/notes'];
-const shouldHideNavbar = hideNavbarPaths.some(path => location.pathname.startsWith(path));
+  const location = useLocation();
+  const hideNavbarPaths = ['/notes'];
+  const shouldHideNavbar = hideNavbarPaths.some(path =>
+    location.pathname.startsWith(path),
+  );
 
-useEffect(() => {
-  const refreshToken = localStorage.getItem("refreshToken");
-  const accessToken = localStorage.getItem("token");
+  useEffect(() => {
+    const refreshToken = localStorage.getItem('refreshToken');
+    const accessToken = localStorage.getItem('token');
 
-  if (!refreshToken) return;
-  dispatch(setAuthToken({ accessToken, refreshToken }));
+    if (!refreshToken) return;
+    dispatch(setAuthToken({ accessToken, refreshToken }));
 
-  dispatch(getUserProfile());
-}, [dispatch]);
+    dispatch(getUserProfile());
+  }, [dispatch]);
 
   return (
     <div>
-        <StarsBg/>
-        <div className='z-5 relative md:px-0 px-4'>
-        {!shouldHideNavbar && <Navbar/>}
-   <Outlet />
-        </div>
+      <StarsBg />
+      <div className="z-5 relative md:px-0 px-4">
+        {!shouldHideNavbar && <Navbar />}
+        <Outlet />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
